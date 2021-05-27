8dbd2474c3334359a7dff696b15d1655cfcd070f
cpoptile <77410366+cpoptile@users.noreply.github.com>
2021-05-27 11:40:41 -0500

    Add progress bar

--- src/views/Dashboard.js
+++ src/views/Dashboard.js
@@ -18,6 +18,8 @@
 import React from "react";
 // react plugin used to create charts
 import { Line, Bar } from "react-chartjs-2";
+import { CircularProgressbar, buildStyles } from 'react-circular-progressbar';
+import 'react-circular-progressbar/dist/styles.css';
 
 // reactstrap components
 import {
@@ -64,8 +66,8 @@ function Dashboard() {
       />
       <div className="content">
         <Row>
-          <Col xs={12} md={4}>
-            <Card className="card-chart">
+          {/* <Col xs={12} md={4}> */}
+            {/* <Card className="card-chart">
               <CardHeader>
                 <h5 className="card-category">Global Sales</h5>
                 <CardTitle tag="h4">Shipped Products</CardTitle>
@@ -101,7 +103,7 @@ function Dashboard() {
                 </div>
               </CardFooter>
             </Card>
-          </Col>
+          </Col> */}
           <Col xs={12} md={4}>
             <Card className="card-chart">
               <CardHeader>
@@ -140,7 +142,35 @@ function Dashboard() {
               </CardFooter>
             </Card>
           </Col>
-          <Col xs={12} md={4}>
+          <Col xs = {12} md = {4}>
+          <CircularProgressbar
+              value={90}
+              text={`${90}%`}
+              styles={buildStyles({
+                // Rotation of path and trail, in number of turns (0-1)
+                rotation: 0.25,
+
+                // Whether to use rounded or flat corners on the ends - can use 'butt' or 'round'
+                strokeLinecap: 'butt',
+
+                // Text size
+                textSize: '16px',
+
+                // How long animation takes to go from one percentage to another, in seconds
+                pathTransitionDuration: 0.5,
+
+                // Can specify path transition in more detail, or remove it entirely
+                // pathTransition: 'none',
+
+                // Colors
+                pathColor: `rgba(62, 152, 199, ${90 / 100})`,
+                textColor: '#f88',
+                trailColor: '#d6d6d6',
+                backgroundColor: '#3e98c7',
+              })}
+            />
+          </Col>
+          {/* <Col xs={12} md={4}>
             <Card className="card-chart">
               <CardHeader>
                 <h5 className="card-category">Email Statistics</h5>
@@ -160,10 +190,10 @@ function Dashboard() {
                 </div>
               </CardFooter>
             </Card>
-          </Col>
+          </Col> */}
         </Row>
         <Row>
-          <Col xs={12} md={6}>
+          {/* <Col xs={12} md={6}>
             <Card className="card-tasks">
               <CardHeader>
                 <h5 className="card-category">Backend Development</h5>
@@ -318,12 +348,13 @@ function Dashboard() {
                 </div>
               </CardFooter>
             </Card>
-          </Col>
+          </Col> */}
+          
           <Col xs={12} md={6}>
             <Card>
               <CardHeader>
-                <h5 className="card-category">All Persons List</h5>
-                <CardTitle tag="h4">Employees Stats</CardTitle>
+                <h5 className="card-category">Transcation History</h5>
+                <CardTitle tag="h4">Recent Transactions</CardTitle>
               </CardHeader>
               <CardBody>
                 <Table responsive>
